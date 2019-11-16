a4c81cdd6dccbd4a45a9621c3f853746ad98f6fb
Ted <xiongjinchao@hotmail.com>
2019-11-16 15:27:38 +0800

    Update commonmark.go

--- commonmark.go
+++ commonmark.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/JohannesKaufmann/html-to-markdown/escape"
+	// "github.com/xiongjinchao/html-to-markdown/escape"
 	"github.com/PuerkitoBio/goquery"
 )
 
